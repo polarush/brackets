1cc1a3e70d91c3c144691538dc583a440c47d017
Yevgeniy Polyarush <polyarush.e@mail.ru>
2021-08-25 16:26:43 +0400

    feat: add main function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	let cases = []
+    
+    for (let i = 0; i < bracketsConfig.length; i++) { 
+    	cases.push(bracketsConfig[i][0] + bracketsConfig[i][1])
+    }
+		
+	for (let i = 0; i < str.length; i++) { 
+		for (let j = 0; j < bracketsConfig.length; j++) { 
+			while (str.includes(cases[j], 0)) {
+            	str = str.replace(cases[j], "");
+			}
+		
+		}
+	}
+	
+	if (str === '()') {  
+		return true
+	}
+
+	else {
+		return str === ''
+	} 
+	
+	
 }
